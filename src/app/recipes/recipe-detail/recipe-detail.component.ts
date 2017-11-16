29cea69dd7ca952e12ffe3667f0675072596256a
koen99 <kjwdamme@avans.nl>
2017-11-16 18:43:13 +0100

    fix college 110

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
 import {Recipe} from '../recipe.model';
+import {RecipeService} from '../recipes.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -10,9 +11,13 @@ import {Recipe} from '../recipe.model';
 export class RecipeDetailComponent implements OnInit {
   @Input() recipe: Recipe;
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+  }
+
+  onAddToShoppingList() {
+    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
   }
 
 }
